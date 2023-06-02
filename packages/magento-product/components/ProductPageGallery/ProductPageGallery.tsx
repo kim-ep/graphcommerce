e1a09b396d2e9b096f477599f5225dc9840d39db
Giovanni Schroevers <giovie2000@hotmail.com>
2023-06-02 13:19:45 +0200

    feat(GCOM-1052): add GridGallery component

--- packages/magento-product/components/ProductPageGallery/ProductPageGallery.tsx
+++ packages/magento-product/components/ProductPageGallery/ProductPageGallery.tsx
@@ -4,6 +4,7 @@ import {
   SidebarGalleryProps,
   TypeRenderer,
 } from '@graphcommerce/next-ui'
+import { GridGallary } from './GridGallary'
 import { ProductPageGalleryFragment } from './ProductPageGallery.gql'
 
 export type ProductPageGalleryRenderers = TypeRenderer<
@@ -21,24 +22,31 @@ export function ProductPageGallery(props: ProductPageGalleryProps) {
     ...sidebarProps
   } = props
 
+  const gallary =
+    media_gallery
+      ?.filter(nonNullable)
+      .sort((a, b) => (a.position ?? 0) - (b.position ?? 0))
+      .map((item) => {
+        if (item.__typename === 'ProductImage')
+          return { src: item.url ?? '', alt: item.label || undefined, width, height }
+        return {
+          src: '',
+          alt: `{${item.__typename} not yet supported}`,
+        }
+      }) ?? []
+
+  const gridGallaryEnabled = true
+
+  if (gridGallaryEnabled) {
+    return <GridGallary {...sidebarProps} sidebar={children} images={gallary} />
+  }
+
   return (
     <SidebarGallery
       {...sidebarProps}
       sidebar={children}
       aspectRatio={[width, height]}
-      images={
-        media_gallery
-          ?.filter(nonNullable)
-          .sort((a, b) => (a.position ?? 0) - (b.position ?? 0))
-          .map((item) => {
-            if (item.__typename === 'ProductImage')
-              return { src: item.url ?? '', alt: item.label || undefined, width, height }
-            return {
-              src: '',
-              alt: `{${item.__typename} not yet supported}`,
-            }
-          }) ?? []
-      }
+      images={gallary}
     />
   )
 }
